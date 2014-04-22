19556e44b6a0a54da11eb5d8d4b92cf3dec77a58
Michael <mbaughman929@gmail.com>
2014-04-21 20:36:07 -0700

    layouts-2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwoLayouts::Application.config.secret_key_base = 'bee07aa6c0c368d1b076311fcdf525cd0b4dbbf54698b6f2a63edcfad19858da4065a51846dc2c8498395930bb2338666b6fd84625d71e20b23282c14bf39c87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwoLayouts::Application.config.session_store :cookie_store, key: '_two layouts_session'
